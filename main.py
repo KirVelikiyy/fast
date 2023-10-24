5f9028025ba5c2d72fbf0f4d18b6ae723343a0b6
KirVelikiy <lisov.kirill50@gmail.com>
2023-10-24 22:04:49 +0400

    add python types

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+class Fruits(Enum):
+    apple = "apple"
+    banana = "banana"
+    strawberry = "strawberry"
 
 
 @app.get("/")
@@ -11,3 +19,15 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/fruits/{fruit}")
+async def get_fruit(fruit: Fruits):
+    default_message = "Your fruit is"
+    match fruit:
+        case Fruits.apple:
+            return {"message": f"{default_message} {Fruits.apple.value}"}
+        case Fruits.banana:
+            return {"message": f"{default_message} {Fruits.banana.value}"}
+        case Fruits.strawberry:
+            return {"message": f"{default_message} {Fruits.strawberry.value}"}
